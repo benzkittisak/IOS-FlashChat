bf020350727be96936b7ff9a2e8d061d6383e64e
benzkittisak <benzkittisak@stv.ac.th>
2022-06-26 13:50:24 +0700

    update code to handle register error

--- FlashChat/Controllers/RegisterViewController.swift
+++ FlashChat/Controllers/RegisterViewController.swift
@@ -6,30 +6,44 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
-
+    
     
     @IBOutlet weak var emailTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        guard let email = emailTF.text else { return print("Error email is nil") }
+        guard let password = passwordTF.text else { return print("Error email is nil") }
+        
+        //        ให้มันสร้าง user กับ email เข้าไปเก็บที่ firestore
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            //            if error not nil
+            if let err = error {
+                //                เรียกใช้ Alert เหมือน alert() ใน js แหละ
+                let alert = UIAlertController(title: "เกิดข้อผิดพลาด", message: err.localizedDescription, preferredStyle: .alert)
+                //                เพิ่มปุ่ม ตกลง ไปที่ alert แล้วก็ถ้ากดตกลงก็ให้ปิด alert
+                alert.addAction(UIAlertAction(title: "ตกลง", style: .default, handler: nil))
+                //                เอา popup ขึ้นไปแสดงผล
+                self.present(alert, animated: true, completion: nil)
+//                print(err.localizedDescription)
+            } else {
+                //            if register success
+                //            Navigate to the ChatViewController
+                self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+            }
+        }
+        
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
