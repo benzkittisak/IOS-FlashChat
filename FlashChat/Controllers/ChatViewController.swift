08a9d432aca7e2ec562e0330fd7ad0dca1bacc6d
benzkittisak <benzkittisak@stv.ac.th>
2022-06-27 11:36:28 +0700

    added retrieve function into our code

--- FlashChat/Controllers/ChatViewController.swift
+++ FlashChat/Controllers/ChatViewController.swift
@@ -13,11 +13,10 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTF: UITextField!
     
-    var messages:[Message] = [
-        Message(sender: "1@2.com", body: "Hey!"),
-        Message(sender: "a@b.com", body: "Hello!"),
-        Message(sender: "1@2.com", body: "What's up?")
-    ]
+    var messages:[Message] = []
+    
+    //    เรียกใช้งาน Firestore
+    let db = Firestore.firestore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,14 +26,63 @@ class ChatViewController: UIViewController {
         
         //        ก็คือให้มันซ่อนปุ่ม กลับ นั่นแหละเพราะว่าถ้าล็อคอินเข้ามาแล้วมันก็ไม่ควรที่จะกลับไปหน้า login ได้แล้ว
         navigationItem.hidesBackButton = true
-
+        
         tableView.dataSource = self
         
-//        ใช้ตัว cell ที่เราสร้างขึ้นมาเอง
+        //        ใช้ตัว cell ที่เราสร้างขึ้นมาเอง
         tableView.register(UINib.init(nibName: Const.cellNibName, bundle: nil), forCellReuseIdentifier: Const.cellIdentifier)
+        
+//        เมื่อหน้าแชทโหลดขึ้นมาเราก็จะให้มันเรียกใช้ฟังก์ชันนี้เพื่อไปดึงข้อมูลแชทจาก firebase มา
+        loadMessage()
     }
     
+    
+    func loadMessage() {
+        messages = []
+        
+//        อ่านข้อมูลจาก firestore
+        db.collection(Const.FStore.collectionName).getDocuments { querySnapshot, error in
+            if let err = error {
+                print("There was an issue retrieving messsage from firestore , \(err)")
+            } else {
+//                querySnapshot มันเป็น array แหละมั่ง ลืมแฮะ
+                for document in querySnapshot!.documents {
+                    let data = document.data()
+                    
+                    guard let messageSender = data[Const.FStore.senderField] as? String else {
+                        return print("Error the sender from firestore is empty or nil") }
+                    
+                    guard let messageBody = data[Const.FStore.bodyField] as? String else { return print("Error the message body from firestore is nil") }
+                    
+                    let newMessage = Message(sender: messageSender, body: messageBody)
+                    self.messages.append(newMessage)
+                }
+            }
+        }
+    }
+    
+    
     @IBAction func sendPressed(_ sender: UIButton) {
+        guard let messageBody = messageTF.text else { return print("Error message body is empty or nil") }
+        
+        //        แล้วเราจะไปเอา ข้อมูลผู้ส่งมาจากไหน เราก็จะไปเอามาจาก firebase แบบนิ้
+        //        เอา email ของคนที่กำลัง login เข้ามาอยู่
+        guard let messageSender = Auth.auth().currentUser?.email else { return print("Error sender is empty or nil") }
+        
+
+        var ref:DocumentReference? = nil
+        //        ต่อไปจะทำการเลือก collection มาใช้แหละ ซึ่งเราจะใช้ชื่อ collection ว่า message
+        ref = db.collection(Const.FStore.collectionName).addDocument(data: [
+            Const.FStore.senderField:messageSender,
+            Const.FStore.bodyField:messageBody
+        ], completion: { error in
+            if let err = error {
+                print("There was an issue saving data to firestore . \(err)")
+            } else {
+                print("Successfully added data")
+            }
+        })
+        
     }
     
     
@@ -54,14 +102,14 @@ class ChatViewController: UIViewController {
 // เราจะเอาข้อมูลไปแสดงผลในตารางแหละ
 extension ChatViewController : UITableViewDataSource {
     
-//    จำนวนแถวในตารางแหละ
+    //    จำนวนแถวในตารางแหละ
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return messages.count
     }
     
-//
+    //
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        สร้าง cell 1 cell น่ะ
+        //        สร้าง cell 1 cell น่ะ
         let cell = tableView.dequeueReusableCell(withIdentifier: Const.cellIdentifier, for: indexPath) as! MessageCell
         cell.lable.text = "\(messages[indexPath.row].body)"
         return cell
